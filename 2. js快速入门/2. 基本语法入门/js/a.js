f1222ab00491fc07fa9327ff0ad3b409d7994ea7
Kerry <2413800399@qq.com>
2021-08-05 17:00:37 +0800

    2021-8-5

---
+++ 2. js快速入门/2. 基本语法入门/js/a.js
@@ -0,0 +1,15 @@
+// 1. 定义变量  变量类型    变量名=变量值
+var score = 1;
+var name = "aa";
+alert(score);
+// 2. 条件控制
+if (score > 60 && score <= 80) {
+    alert("60-80");
+} else if (score > 80) {
+    alert("80-100");
+} else if (score < 60) {
+    alert("0-60");
+}
+
+
+// console.log(score)   在浏览器控制台打印变量
